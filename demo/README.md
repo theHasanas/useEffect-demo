17ab8daea0e3f2a17c99fb88322c2a66c1fe8831
theHasanas <30086691+theHasanas@users.noreply.github.com>
2020-09-29 00:08:54 +0300

    Add demo scaffold

--- demo/README.md
+++ demo/README.md
@@ -1,2 +1,5 @@
 # Demo
+
+[Presentation slides](https://docs.google.com/presentation/d/19VNDgow5ZntpEfkLzLRjvKK_V-Kp8w1NfM70utwtFjo/edit?usp=sharing)
+
 Prepared by Hasanas.
--- demo/src/App.js
+++ demo/src/App.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        Hello
-      </header>
+    <div>
+      <p>Introduce yourself</p>
+
+      <div>
+        <label>First name: </label>
+        <input />
+      </div>
+
+      <div>
+        <label>Last name: </label>
+        <input />
+      </div>
+
+      <div>
+        <label>Age: </label>
+        <input />
+      </div>
     </div>
   );
 }
