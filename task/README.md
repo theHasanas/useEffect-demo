3ef990f4557e79e2ca410b2434ac18073568ba74
theHasanas <30086691+theHasanas@users.noreply.github.com>
2020-10-04 11:18:01 +0300

    Prepare task code and text

--- task/README.md
+++ task/README.md
@@ -1,1 +1,27 @@
 # Task
+
+You are presented with a very basic app and tasked to make a couple
+of improvements to make the user experience better.
+
+## Installation
+
+Run `npm install` in `/useEffect-demo/task/`;
+
+## Running
+
+Run `npm start` in `/useEffect-demo/task/`;
+
+## Improvement I
+
+After clicking "Convert" there is a delay before the converted amount is displayed.
+
+Let's fix this by displaying "Loading..." in the textbox until the converted
+amount is ready to be displayed.
+
+## Improvement II
+
+Clicking is tiresome. Let's help the user by automatically converting the amount
+as soon as they finish typing it. Be aware that if you make too many API calls
+in a short period of time the server might block your subsequent requests.
+
+Hint: WAIT until the user has not typed for at least a second.
--- task/src/App.js
+++ task/src/App.js
@@ -1,24 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
+
+const frankfurterAPI = "api.frankfurter.app";
+const EUR = "EUR";
+const GBP = "GBP";
 
 function App() {
+  const [currency, setCurrency] = useState(EUR);
+  const [amount, setAmount] = useState(1);
+  const [convertedAmount, setConvertedAmount] = useState(0);
+
+  function handleCurrencyChange(event) {
+    setCurrency(event.currentTarget.value);
+  }
+
+  function handleAmountChange(event) {
+    if (event.target.validity.valid)
+      setAmount(parseInt(event.target.value) || 0);
+  }
+
+  function convertAmount() {
+    fetch(
+      `https://${frankfurterAPI}/latest?amount=${amount}&from=${currency}&to=USD`
+    )
+      .then((resp) => resp.json())
+      .then((data) => {
+        setConvertedAmount(data.rates.USD);
+      });
+  }
+
   return (
     <div>
-      <p>Introduce yourself</p>
+      <h1>EXCHANGED</h1>
 
       <div>
-        <label>First name: </label>
-        <input />
+        <h4>Currency</h4>
+        <label>
+          <span role="img" aria-label="European Union flag">
+            🇪🇺
+          </span>
+          <input
+            type="radio"
+            value={EUR}
+            checked={currency === EUR}
+            onChange={handleCurrencyChange}
+          />
+        </label>
+        <label>
+          <span role="img" aria-label="Union Jack flag">
+            🇬🇧
+          </span>
+          <input
+            type="radio"
+            value={GBP}
+            checked={currency === GBP}
+            onChange={handleCurrencyChange}
+          />
+        </label>
+
+        <input value={amount} onChange={handleAmountChange} pattern="[0-9]*" />
       </div>
 
       <div>
-        <label>Last name: </label>
-        <input />
+        <h4>In US Dollars</h4>
+        <p>
+          <span role="img" aria-label="United States flag">
+            🇺🇸
+          </span>
+          <input readOnly={true} value={convertedAmount} />
+        </p>
       </div>
-
-      <div>
-        <label>Age: </label>
-        <input />
-      </div>
+      <button onClick={convertAmount}>Convert</button>
     </div>
   );
 }
